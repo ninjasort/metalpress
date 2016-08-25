e6af3c0da95e26e17b11272b92d0fd016ca94b2e
Cameron J Roe <cameronjroe@gmail.com>
2016-08-25 17:00:18 -0400

    fix(metadata): fix pagemetadata in pagination

--- src/index.js
+++ src/index.js
@@ -141,7 +141,7 @@ export default function (config = {}, callback) {
         if (files[file].collection.length) {
           let collection = files[file].collection[0];
           let layout = config.pagination[`collections.${collection}`].layout;
-          if (config.pagination[`collections.${collection}`].pageMetadata.title) {
+          if (config.pagination[`collections.${collection}`].pageMetadata && config.pagination[`collections.${collection}`].pageMetadata.title) {
             files[file].title = config.pagination[`collections.${collection}`].pageMetadata.title;
           }
           files[file].layout = layout;
