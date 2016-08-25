3be041c6183d68fdec638361700a1b62ff6c8e3e
Cameron J Roe <cameronjroe@gmail.com>
2016-08-25 16:39:31 -0400

    fix(tests): stop running tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,11 +1,11 @@
-import { expect } from 'chai';
-import sinon from 'sinon';
-import { loadJsOrYaml } from '../src/util/fs';
+// import { expect } from 'chai';
+// import sinon from 'sinon';
+// import { loadJsOrYaml } from '../src/util/fs';
 
-describe('loadJsOrYaml', () => {
+// describe('loadJsOrYaml', () => {
   
-  // it('should load correctly', () => {
-  //   loadJsOrYaml('/data/blog.yaml');
-  // });
+//   // it('should load correctly', () => {
+//   //   loadJsOrYaml('/data/blog.yaml');
+//   // });
 
-});
+// });
