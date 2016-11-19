56952056419a114b5374ebfb7565fab665930ed0
Cameron J Roe <cameronjroe@gmail.com>
2016-11-18 22:32:12 -0500

    style(semicolons): remove all semicolons

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-import path             from 'path';
-import fs               from 'fs';
-import Metalsmith       from 'metalsmith';
-import deepAssign       from 'deep-assign';
-import collections      from 'metalsmith-collections';
-import metadata         from 'metalsmith-metadata';
-import markdown         from 'metalsmith-markdown';
-import layouts          from 'metalsmith-layouts';
-import inPlace          from 'metalsmith-in-place';
-import permalinks       from 'metalsmith-permalinks';
-import pagination       from 'metalsmith-pagination';
-import excerpts         from 'metalsmith-better-excerpts';
-import sass             from 'metalsmith-sass';
-import moment           from 'moment';
-import define           from 'metalsmith-define';
-import jekyllDates      from 'metalsmith-jekyll-dates';
-import autoprefixer     from 'metalsmith-autoprefixer';
-import webpack          from 'metalsmith-webpack';
-import ignore           from 'metalsmith-ignore';
-import metallic         from 'metalsmith-metallic';
-import tags             from 'metalsmith-tags';
-import snippet          from 'metalsmith-snippet';
-import blc              from 'metalsmith-broken-link-checker';
-import date             from 'metalsmith-build-date';
-import robots           from 'metalsmith-robots';
-import contentful       from 'contentful-metalsmith';
+import path             from 'path'
+import fs               from 'fs'
+import Metalsmith       from 'metalsmith'
+import deepAssign       from 'deep-assign'
+import collections      from 'metalsmith-collections'
+import metadata         from 'metalsmith-metadata'
+import markdown         from 'metalsmith-markdown'
+import layouts          from 'metalsmith-layouts'
+import inPlace          from 'metalsmith-in-place'
+import permalinks       from 'metalsmith-permalinks'
+import pagination       from 'metalsmith-pagination'
+import excerpts         from 'metalsmith-better-excerpts'
+import sass             from 'metalsmith-sass'
+import moment           from 'moment'
+import define           from 'metalsmith-define'
+import jekyllDates      from 'metalsmith-jekyll-dates'
+import autoprefixer     from 'metalsmith-autoprefixer'
+import webpack          from 'metalsmith-webpack'
+import ignore           from 'metalsmith-ignore'
+import metallic         from 'metalsmith-metallic'
+import tags             from 'metalsmith-tags'
+import snippet          from 'metalsmith-snippet'
+import blc              from 'metalsmith-broken-link-checker'
+import date             from 'metalsmith-build-date'
+import robots           from 'metalsmith-robots'
+import contentful       from 'contentful-metalsmith'
 
 // prod
-import htmlMinifier     from 'metalsmith-html-minifier';
-import fingerprint      from 'metalsmith-fingerprint';
-import imagemin         from 'metalsmith-imagemin';
-import sitemap          from 'metalsmith-sitemap';
-import firebase, {transform} from 'metalsmith-firebase';
-import rss              from 'metalsmith-rss';
-import drafts           from 'metalsmith-drafts';
+import htmlMinifier     from 'metalsmith-html-minifier'
+import fingerprint      from 'metalsmith-fingerprint'
+import imagemin         from 'metalsmith-imagemin'
+import sitemap          from 'metalsmith-sitemap'
+import firebase, {transform} from 'metalsmith-firebase'
+import rss              from 'metalsmith-rss'
+import drafts           from 'metalsmith-drafts'
 
-import createDefaults   from './config/defaults';
+import createDefaults   from './config/defaults'
 import {
   _fixPaginationQuirk,
   _fixPaginationObject
@@ -42,119 +42,121 @@ import {
 
 export default function (config = {}, callback) {
 
-  let options = createDefaults(config);
+  let options = createDefaults(config)
 
   // Config
   // --------------------------------------------------------------------------
-  const m = new Metalsmith(options.basePath);
+  const m = new Metalsmith(
+    path.resolve(process.cwd(), options.basePath)
+  )
 
   // Metalsmith options
   // --------------------------------------------------------------------------
   m.clean(true);
-  m.destination(config.destination || 'dist');
+  m.destination(config.destination || 'dist')
 
   // Object Metadata
   // --------------------------------------------------------------------------
-  m.metadata(options.metadata);
+  m.metadata(options.metadata)
 
   // File Metadata
   // --------------------------------------------------------------------------
-  m.use(metadata(options.filedata));
+  m.use(metadata(options.filedata))
 
   // Build Date
   // --------------------------------------------------------------------------
-  m.use(date({ key: 'dateBuilt' }));
+  m.use(date({ key: 'dateBuilt' }))
   
   // Firebase
   // --------------------------------------------------------------------------
   if (options.firebase) {
-    m.use(firebase(options.firebase));
-    m.use(transform(options.firebase.options));
+    m.use(firebase(options.firebase))
+    m.use(transform(options.firebase.options))
   }
 
   // Contentful
   // --------------------------------------------------------------------------
   if (options.contentful) {
-    m.use(contentful(options.contentful));
+    m.use(contentful(options.contentful))
     m.use((files, m, next) => {
       for(const file in files) {
         try {
-          let data = files[file].data.fields.content;
-          files[file].contents = Buffer.from(data, 'utf8');
+          let data = files[file].data.fields.content
+          files[file].contents = Buffer.from(data, 'utf8')
         } catch (e) { /* skip file */ }
       }
-      next();
+      next()
     })
   }
 
   // Ignores
   // --------------------------------------------------------------------------
-  m.use(ignore(options.ignore));
+  m.use(ignore(options.ignore))
 
   // Drafts
   // --------------------------------------------------------------------------
   if (m.metadata().production) {
-    m.use(drafts());
+    m.use(drafts())
   }
 
   // Definitions
   // --------------------------------------------------------------------------
-  m.use(define({ moment }));
+  m.use(define({ moment }))
 
   // Date
   // --------------------------------------------------------------------------
-  m.use(jekyllDates());
+  m.use(jekyllDates())
 
   // Tags
   // --------------------------------------------------------------------------
   if (options.tags) {
-    m.use(tags(options.tags));
+    m.use(tags(options.tags))
   }
 
   // Code Highlighting
   // --------------------------------------------------------------------------
-  m.use(metallic());
+  m.use(metallic())
 
   // Markdown
   // --------------------------------------------------------------------------
-  m.use(markdown(options.markdown));
+  m.use(markdown(options.markdown))
 
   // Excerpts
   // --------------------------------------------------------------------------
-  m.use(excerpts(options.excerpts));
+  m.use(excerpts(options.excerpts))
 
   // Attach Collections
   // --------------------------------------------------------------------------
-  m.use(collections(options.collections));
+  m.use(collections(options.collections))
   
   // Pagination
   // --------------------------------------------------------------------------
   if (options.pagination) {
-    _fixPaginationQuirk(config);
-    m.use(pagination(options.pagination));
-    m.use(_fixPaginationObject(config));
+    _fixPaginationQuirk(config)
+    m.use(pagination(options.pagination))
+    m.use(_fixPaginationObject(config))
   }
 
   // Permalinks
   // --------------------------------------------------------------------------
-  m.use(permalinks(options.permalinks));
+  m.use(permalinks(options.permalinks))
 
   // Styles
   // --------------------------------------------------------------------------
   if (m.metadata().production) {
-    options.sass.outputStyle = 'compressed';
-    options.sass.sourceMap = false;
-    options.sass.sourceMapEmbed = false;
-    m.use(sass(options.sass));
+    options.sass.outputStyle = 'compressed'
+    options.sass.sourceMap = false
+    options.sass.sourceMapEmbed = false
+    m.use(sass(options.sass))
   } else {
-    m.use(sass(options.sass));
+    m.use(sass(options.sass))
   }
-  m.use(autoprefixer({ browsers: ['last 2 versions'] }));
+  m.use(autoprefixer({ browsers: ['last 2 versions'] }))
 
   // Fingerprinting
   // --------------------------------------------------------------------------
   if (options.fingerprint) {
-    m.use(fingerprint(options.fingerprint));
+    m.use(fingerprint(options.fingerprint))
   }
 
   // Post-template Middleware
@@ -162,51 +164,51 @@ export default function (config = {}, callback) {
   if (options.preMiddleware) {
     for (const plugin in options.preMiddleware) {
       if (options.preMiddleware[plugin]) {
-        m.use(options.preMiddleware[plugin]);
+        m.use(options.preMiddleware[plugin])
       }
     }
   }
 
   // Templates
   // --------------------------------------------------------------------------
-  m.use(layouts(options.layouts));
-  m.use(inPlace(options.inPlace));
+  m.use(layouts(options.layouts))
+  m.use(inPlace(options.inPlace))
 
   // Js
   // --------------------------------------------------------------------------
   if (options.webpack) {
     if (m.metadata().production) {
-      m.use(webpack(options.webpack.prod));
+      m.use(webpack(options.webpack.prod))
     } else {
-      m.use(webpack(options.webpack.dev));
+      m.use(webpack(options.webpack.dev))
     }
   }
 
   // Sitemap
   // --------------------------------------------------------------------------
   if (m.metadata().production && options.sitemap) {
-    m.use(sitemap(options.sitemap));
+    m.use(sitemap(options.sitemap))
   }
 
   // Robots
   // --------------------------------------------------------------------------
   if (options.robots) {
-    m.use(robots(options.robots));
+    m.use(robots(options.robots))
   }
 
   // RSS Feed
   // --------------------------------------------------------------------------
   if (options.rss) {
     if (m.metadata().production) {
-      m.use(rss(options.rss));
+      m.use(rss(options.rss))
     }
   }
 
   // Production
   // --------------------------------------------------------------------------
   if (m.metadata().production) {
-    m.use(imagemin(options.imagemin));
-    m.use(htmlMinifier('*.html', options.htmlMinifier));
+    m.use(imagemin(options.imagemin))
+    m.use(htmlMinifier('*.html', options.htmlMinifier))
   }
 
   // Post-template Middleware
@@ -214,13 +216,13 @@ export default function (config = {}, callback) {
   if (options.postMiddleware) {
     for (const plugin in options.postMiddleware) {
       if (options.postMiddleware[plugin]) {
-        m.use(options.postMiddleware[plugin]);
+        m.use(options.postMiddleware[plugin])
       }
     }
   }
 
-  m.build(callback);
+  m.build(callback)
 
-  return m;
+  return m
 
 }
